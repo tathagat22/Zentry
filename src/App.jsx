aeb187a9b925ae398f1499fc335b804258eed07c
Tathagat Maitray <56931425+tathagat22@users.noreply.github.com>
2025-04-12 05:49:36 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Hero from './components/hero'
+import Hero from './components/Hero'
 import About from './components/About'
 import Navbar from './components/Navbar'
 import Features from './components/Features'
